ccc4a52f1b097c19f666abf1f1269f3c47444395
nowaveosu <nowaveosu@gmail.com>
2023-04-07 16:46:25 +0900

    crud done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import "./index.css";
 
 let todoItemId = 0;
 
