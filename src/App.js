6a383e1b7b03dc99ce0ab066ee6650aa30217c90
nowaveosu <nowaveosu@gmail.com>
2023-04-11 23:48:53 +0900

    4.11

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Button from '@mui/material/Button';
 
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
+  //아이템 입력할 리액트 컴포넌트만들기
+  const onSubmit = () => {
+    props.onSubmit(input);
+    setInput("");
+  };
 
   return (
     <div>
@@ -14,15 +19,26 @@ const TodoItemInputField = (props) => {
         variant = "outlined"
         onChange = {(e) => setInput(e.target.value)} value={input}
       />
-      <Button variant='outlined'>Submit</Button>
+      <Button variant='outlined' onClick={onSubmit}>Submit</Button>
     </div>
   )
 }
 
+const TodoItemList = (props) => {
+  const todoList = props.todoItemList.map((todoItem,index) => {
+    return <li key={index}>{todoItem.todoItemContent}</li>
+  })
+  return(<div>
+    <ul>{todoList}</ul>
+  </div>)
+}
+
 function App() {
+  const [todoItemList, setTodoItemList] = useState([]);
   return (
     <div className='App'>
-      <TodoItemInputField />
+      <TodoItemInputField onSubmit={() => {}} />
+      <TodoItemList todoItemList={[todoItemList]}/>
     </div>
   )
 }
